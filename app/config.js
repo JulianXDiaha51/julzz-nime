8a12dcee1f8c7cb3eea1337ad29dbacc4931fbcc
JulzPiw <126005230+JulianXDiaha51@users.noreply.github.com>
2025-11-23 20:14:58 +0700

    Add files via upload

---
+++ app/config.js
@@ -0,0 +1,28 @@
+// jangan di ganti
+export const Api = "https://www.sankavollerei.com/anime/samehadaku"
+
+export const Config = {
+  // Nama website Anda, akan muncul di Hero Section dan judul tab browser
+  name: "Julzznime",
+  name2: "JulzzNime - Nonton Anime Gratis",
+  author: "JulianDev",
+
+  // Deskripsi singkat website Anda
+  description: "JulzzNime adalah situs streaming anime gratis tanpa iklan, tempat kamu bisa menonton anime dengan nyaman dan kualitas terbaik",
+
+  // URL untuk logo
+  logo: "https://cdn.odzre.my.id/1eh.jpg",
+
+  donateLink: "https://qris.zone.id/juliandev",
+
+  pages: {
+    notFound: {
+      title: "Wah, Tersesat di Isekai? 🗺️",
+      logo: "https://cihuy.biz.id/file/_xbA5BeZ.png",
+      description: "Halaman yang kamu cari sepertinya tidak ada di dimensi ini. Ayo kembali ke jalan yang benar!"
+    },
+    Footer: {
+      about: "Website ini tidak menyimpan file apa pun di servernya. Semua konten disediakan oleh pihak ketiga yang tidak berafiliasi."
+    }
+  }
+};
